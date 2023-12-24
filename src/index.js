e99e248a64135132adb0a87f61b9a79a14275d5b
adityasharma2020 <aditya15116617@gmail.com>
2023-12-25 02:23:29 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@
 const express = require('express');
 const { PORT, JWT_KEY } = require('./config/server-config');
 const apiRoutes = require('./routes/index');
-
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // To parse incoming req. body
-
 const prepareAndStartServer = () => {
 	app.use('/api', apiRoutes);
 
 	app.listen(PORT, async () => {
-		
 		console.log(`successfully started the server on port : ${PORT}`);
 	});
 };
